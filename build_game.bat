00d11e174a091b1dd7565921cd372d82667193f1
hmkey <hmkchan0327@gmail.com>
2015-04-19 04:13:45 -0500

    Wanted me to commit twice bc failed
    For some reason it made me commit twice bc the snyc for Added an island
    failed.

--- build_game.bat
+++ build_game.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 
-set DRIVE_LETTER=C:
+set DRIVE_LETTER=%1:
 set GAME_PATH=%DRIVE_LETTER%
-set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;%GAME_PATH%\Ogre\bin;%GAME_PATH%\boost\bin;%GAME_PATH%\Ogre\ogredeps\bin;%GAME_PATH%\bass_2.4;%GAME_PATH%\MyGUI_3.2.2\bin;%GAME_PATH%\lua_5.3.0\bin;c:\Windows;c:\Windows\system32
+set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;%GAME_PATH%\Ogre\bin;%GAME_PATH%\boost\bin;%GAME_PATH%\Ogre\ogredeps\bin;c:\Windows;c:\Windows\system32
 
 mingw32-make GAME_DRIVE_LETTER="%DRIVE_LETTER%"
