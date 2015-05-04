9f7a3cc2c257ca75e0c906a303fe8c5115ad01dd
Philip Westrich <pwestrich@me.com>
2015-05-03 23:19:14 -0500

    Sometimes the boat clips through the world. I don't understand.

--- client/assets/lua/buttonNavigate.lua
+++ client/assets/lua/buttonNavigate.lua
@@ -1,4 +1,4 @@
 
 --	void startAutopilot(objectName, length, vfx, vfy, vfz, lfx, lfy, lfz);
 
-Scene:startAutopilot("MiniMerryTrasnformNode", 30.0, 0.0, 0.0, 0.0, 100.0, 100.0, 100.0)
+Scene:startAutopilot("MiniMerryTrasnformNode", 30.0, 0.0, 0.0, 0.0, 10.0, 10.0, 100.0)
